2bcd68cedb39204f4465b67d387fcd0d4cc9b487
larcohex <kpirimbaev@optimal-dynamics.com>
2016-05-04 14:12:51 +0600

    Changed to `/var/lib` in `djinn.rb`

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -4545,9 +4545,9 @@ HOSTS
       'EC2_HOME' => ENV['EC2_HOME'],
       'JAVA_HOME' => ENV['JAVA_HOME']
     }
-    start = "/usr/bin/ruby -w /root/appscale/AppController/djinnServer.rb"
+    start = "/usr/bin/ruby -w /var/lib/appscale/AppController/djinnServer.rb"
     stop = "/usr/sbin/service appscale-controller stop"
-    match_cmd = "/usr/bin/ruby -w /root/appscale/AppController/djinnServer.rb"
+    match_cmd = "/usr/bin/ruby -w /var/lib/appscale/AppController/djinnServer.rb"
 
     # Let's make sure we don't have 2 jobs monitoring the controller.
     FileUtils.rm_rf("/etc/monit/conf.d/controller-17443.cfg")
